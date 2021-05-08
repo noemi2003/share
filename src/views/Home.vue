19450803669ae04038663a0f9489b1ee251a0474
noemi <kuramotonoemi@kuramotonowarainoMacBook-Air.local>
2021-05-08 14:49:30 +0900

    homevue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,55 +1,24 @@
 <template>
   <div class="flex">
     <div class="left">
-      <div class="left-padding">
-        <img class="logo" src="../assets/logo.png" />
-        <div class="flex icon-field">
-          <img class="icon" src="../assets/home.png" />
-          <p>ホーム</p>
-        </div>
-        <div class="flex icon-field">
-          <img class="icon" src="../assets/profile.png" />
-          <p>プロフィール</p>
-        </div>
-        <div class="flex icon-field">
-          <img class="icon" src="../assets/logout.png" />
-          <p>ログアウト</p>
-        </div>
-      </div>
-      <div class="share">
-        <p>シェア</p>
-        <textarea></textarea>
-        <div>
-          <button>シェアする</button>
-        </div>
-      </div>
+      <SideNavi />
     </div>
     <div class="right">
       <div class="title">
         <p>ホーム</p>
       </div>
-      <div v-for="(value, index) in shares" :key="index">
-        <div class="message">
-          <div class="flex">
-            <p class="name">{{value.name}}</p>
-            <img class="icon" src="../assets/heart.png" />
-            <p class="number">{{value.like.length}}</p>
-            <img class="icon" src="../assets/cross.png" />
-            <img class="icon detail" src="../assets/detail.png" />
-          </div>
-          <p class="text">{{value.share}}</p>
-        </div>
-      </div>
+      <Message />
     </div>
   </div>
 </template>
 
 <script>
+import SideNavi from "../components/SideNavi";
+import Message from "../components/Message";
 export default {
-  data() {
-    return {
-      shares: [{ name: "太郎", like: [], share: "初めまして" }]
-    };
+  components: {
+    SideNavi,
+    Message
   }
 };
 </script>
@@ -74,73 +43,5 @@ export default {
 .title p {
   font-size: 20px;
   font-weight: bold;
-}
-.left-padding {
- margin: 20px;
-}
-.logo {
- width: 100px;
-}
-.icon-field {
- margin-top: 15px;
- cursor: pointer;
-}
-.icon-field p {
- font-size: 16px;
- padding-left: 15px;
-}
-.share {
- margin: 15px;
-}
-.share textarea {
- width: 95%;
- height: 120px;
- margin-top: 15px;
- margin-bottom: 15px;
- border-radius: 10px;
- border: 1px solid white;
- background-color: #15202b;
- color: white;
- resize: none;
-}
-button {
- width: 100px;
- text-align: center;
- padding: 8px 0 10px;
- color: #fff;
- background-color: #5419da;
- border-radius: 25px;
- display: block;
- margin: 0 0 0 auto;
-}
-.header {
- text-align: center;
- margin-top: 20px;
- padding-bottom: 10px;
- border-bottom: 1px solid white;
-}
-.icon {
- width: 25px;
- height: 25px;
-}
-.detail {
- margin-left: 50px;
-}
-.message {
- padding: 20px;
- border-bottom: solid 1px white;
- border-left: solid 1px white;
-}
-.name {
- font-size: 18px;
- font-weight: bold;
- margin-right: 10px;
-}
-.text {
- margin-top: 10px;
-}
-.number {
- margin-left: 10px;
- margin-right: 10px;
 }
 </style>
